5f8e2d4ac95fe0c59754cbb0bf96f4d5baebd489
Shragi Russell <113606597+Shragi-Russell@users.noreply.github.com>
2025-05-11 13:04:56 +0300

    Create quotes.py
    Creating some quotes (kept as a separate file for neatness).

---
+++ quotes.py
@@ -0,0 +1,7 @@
+# List of motivational quotes
+quotes = [
+    "Believe you can and you're halfway there.",
+    "Success is not the key to happiness. Happiness is the key to success.",
+    "Don't watch the clock; do what it does. Keep going.",
+    "The only way to do great work is to love what you do.",
+    "The future belongs to those who believe in the beauty of their dreams.",
